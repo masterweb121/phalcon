f3e99eaf1602654b62ac90597e3ee4d97a58396d
Neo Chen <netkiller@msn.com>
2014-02-12 17:38:13 +0800

    add config file

--- public/index.php
+++ public/index.php
@@ -3,23 +3,34 @@
 
 try {
 
+
+    //Read the configuration
+    $config = new Phalcon\Config\Adapter\Ini('../app/config/config.ini');
+
     //Register an autoloader
     $loader = new \Phalcon\Loader();
-    $loader->registerDirs(array(
-        '../app/controllers/',
-        '../app/models/'
-    ))->register();
+    $loader->registerDirs(
+        array(
+            $config->application->controllersDir,
+            $config->application->pluginsDir,
+            $config->application->libraryDir,
+            $config->application->modelsDir,
+        )
+    )->register();
 
     //Create a DI
+    // The FactoryDefault Dependency Injector automatically registers the
+    // right services providing a full-stack framework
     $di = new Phalcon\DI\FactoryDefault();
 
     //Setup the database service
-    $di->set('db', function(){
+    // Database connection is created based on parameters defined in the configuration file
+    $di->set('db', function() use ($config) {
         return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
-            "host" => "localhost",
-            "username" => "www",
-            "password" => "chen",
-            "dbname" => "test"
+            "host" => $config->database->host,
+            "username" => $config->database->username,
+            "password" => $config->database->password,
+            "dbname" => $config->database->name
         ));
     });
 
@@ -35,6 +46,13 @@ try {
         $url = new \Phalcon\Mvc\Url();
         $url->setBaseUri('/');
         return $url;
+    });
+
+    //Start the session the first time a component requests the session service
+    $di->set('session', function() {
+        $session = new Phalcon\Session\Adapter\Files();
+        $session->start();
+        return $session;
     });
 
     //Handle the request
